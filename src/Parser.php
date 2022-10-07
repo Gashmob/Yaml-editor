6c353b8e6b3f119c1186c3e1b3371d0130b4024d
Kevin Traini <kevin@ktraini.com>
2022-10-07 21:11:04 +0200

    Add Parser::prepare

--- src/Parser.php
+++ src/Parser.php
@@ -5,11 +5,40 @@ namespace Gashmob\YamlEditor;
 class Parser
 {
     /**
+     * Parse a string formatted as YAML into a PHP array.
+     *
      * @param string $input
      * @return array
      */
     public function parse($input)
     {
+        $lines = $this->prepare($input);
+
         return [];
+    }
+
+    // _.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-.
+
+    /**
+     * Prepare the input string for parsing.
+     * - Explode on \n
+     * - rtrim
+     * - remove empty lines
+     *
+     * @param string $input
+     * @return array
+     */
+    private function prepare($input)
+    {
+        $result = explode("\n", $input);
+
+        foreach ($result as $key => $line) {
+            $result[$key] = rtrim($line);
+            if (empty($result[$key])) {
+                unset($result[$key]);
+            }
+        }
+
+        return $result;
     }
 }

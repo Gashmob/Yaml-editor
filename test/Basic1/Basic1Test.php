ad1a69ca7ac7bce19811f3746a8f6dd2d1e52c53
Kevin Traini <kevin@ktraini.com>
2022-10-07 10:38:07 +0200

    Create a basic test doing nothing

---
+++ test/Basic1/Basic1Test.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Gashmob\YamlEditor\Test\Basic1;
+
+use Gashmob\YamlEditor\Test\Test;
+
+class Basic1Test implements Test
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function run()
+    {
+        return true;
+    }
+}
